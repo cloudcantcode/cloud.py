74e27064b52935abcccbf930e90c68477318cffa
sleepy <eskay666pog@gmail.com>
2020-04-27 04:07:48 +0100

    Made scanning for cogs function more efficient.

--- bot.py
+++ bot.py
@@ -70,4 +70,4 @@ for filename in os.listdir('./cogs'):
     if filename.endswith('.py'):
         bot.load_extension(f'cogs.{filename[:-3]}')
 
-bot.run('your token here')
+bot.run('Your token here')
